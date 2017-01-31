a1319644e5aa152d9b46d73061de7ba7f480d6c9
Hisham Abi Farah <hisham.abi.farah@gmail.com>
2017-01-31 13:54:15 +0200

    first commit

---
+++ app/src/main/java/com/inci/androidapisandthirdpartylibrairies/Retrofit/RetrofitBasicExample.java
@@ -0,0 +1,76 @@
+package com.inci.androidapisandthirdpartylibrairies.Retrofit;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.inci.androidapisandthirdpartylibrairies.R;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+/**
+ * Created by Hisham Abi Farah on 1/29/2017.
+ */
+
+public class RetrofitBasicExample extends AppCompatActivity  {
+    private static final String TAG = "RetrofitBasicExample";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        this.loadUser();
+    }
+
+    public void loadUser(){
+        Gson gson = new GsonBuilder()
+                .setDateFormat("yyyy-MM-dd'T'HH:mm:ssZ")
+                .create();
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(GithubAPI.ENDPOINT)
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build();
+        GithubAPI githubUserAPI = retrofit.create(GithubAPI.class);
+
+        Call<GithubUser> callUser = githubUserAPI.getUser("hisham-abifarah");
+        //asynchronous call
+        callUser.enqueue(new Callback<GithubUser>() {
+
+        @Override
+        public void onResponse(Call<GithubUser> call, Response<GithubUser> response) {
+            int code = response.code();
+            if (code == 200) {
+                GithubUser user = response.body();
+                Log.d(TAG, "onResponse: " + "Username: " + user.login + "\t" + " User Id: " + user.id );
+            } else {
+                Log.d(TAG, "onResponse: Failed to get User Details");
+            }
+        }
+        @Override
+        public void onFailure(Call<GithubUser> call, Throwable t) {
+            Log.d(TAG, "onFailure: Failed to get User Details");
+        }});
+    }
+}
+
+    class GithubUser {
+
+     String login;
+     String id;
+}
+     interface GithubAPI {
+        String ENDPOINT = "https://api.github.com";
+
+        @GET("/users/{user}")
+        Call<GithubUser> getUser(@Path("user") String user);
+}
---
+++ app/src/main/java/com/inci/androidapisandthirdpartylibrairies/Volley/VolleyBasicExample.java
@@ -0,0 +1,76 @@
+package com.inci.androidapisandthirdpartylibrairies.Volley;
+
+import android.content.res.Resources;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.inci.androidapisandthirdpartylibrairies.R;
+
+import org.json.JSONObject;
+
+/**
+ * Created by Hisham Abi Farah on 1/29/2017.
+ */
+
+public class VolleyBasicExample extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    private TextView txtResponse= null;
+    String url = "http://headers.jsontest.com/";
+    Resources resources;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        txtResponse= (TextView)findViewById(R.id.textView);
+        resources = getResources();
+        this.volleyJsonObjectRequest(url);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main,menu);
+        return true;
+    }
+
+    public void volleyJsonObjectRequest(String url){
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+
+        JsonObjectRequest jsObjRequest = new JsonObjectRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.d(TAG, "onResponse: " + response.toString()) ;
+                        String textResponse = resources.getString(R.string.textview_response,response.toString());
+                        txtResponse.setText(textResponse);
+                        findViewById(R.id.progressBar).setVisibility(View.GONE);
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d(TAG, "onErrorResponse: " + error.getMessage());
+                        String textResponse = resources.getString(R.string.textview_response,error.toString());
+                        txtResponse.setText(textResponse);
+                        findViewById(R.id.progressBar).setVisibility(View.GONE);
+                    }
+                });
+        queue.add(jsObjRequest);
+    }
+}
